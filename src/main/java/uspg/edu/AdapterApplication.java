bee3bf611efc2c244a52e6c47b96f3be2c17f40d
MarioZavala2000123 <mrzavalat@alumno.uspg.edu.gt>
2021-10-09 12:06:56 -0600

    menu principal

--- src/main/java/uspg/edu/AdapterApplication.java
+++ src/main/java/uspg/edu/AdapterApplication.java
@@ -17,85 +17,20 @@ import edu.uspg.model.Alumno;
 @SpringBootApplication
 public class AdapterApplication {
 
+	static int carnet;
+	static String nombres;
+	static String apellidos;
+	static String correo;
+	static int opcion = 0;
+
+	static Scanner scn = new Scanner(System.in);
+	static ArrayList<Alumno> listaAlumno = new ArrayList();
+
+	static Alumno alumno;
+
 	public static void main(String[] args) {
 		SpringApplication.run(AdapterApplication.class, args);
-
-		Scanner scn = new Scanner(System.in);
-
-		ArrayList<Alumno> listaAlumno = new ArrayList();
-
-		Alumno alumno = new Alumno();
-
-		Alumno alumno2 = new Alumno();
-
-		int carnet;
-		String nombres;
-		String apellidos;
-		String correo;
-
-		ListaAlumnos listaAlumnos = new ListaAlumnos();
-
-		
-
-		int opcion = 0;
-
-		do {
-
-			System.out.println(" ");
-			System.out.println("\t ---Menú para registrar datos---");
-			System.out.println("Escriba el número de la acción a realizar");
-			System.out.println("1. Ingresar datos Alumnos");
-			System.out.println("2. Mostrar datos en XML");
-			System.out.println("3. Mostrar datos en JSON");
-			System.out.println("4. Salir");
-			System.out.print("--Opción:");
-			opcion = scn.nextInt();
-
-			switch (opcion) {
-
-			case 1:
-
-				System.out.println("Por favor ingrese los datos solicitados:");
-
-				System.out.println("   Ingrese carnet: ");
-				System.out.print(" -");
-				carnet = scn.nextInt();
-
-				System.out.println("   Ingrese nombre: ");
-				System.out.print(" -");
-				nombres = scn.next();
-
-				System.out.println("   Ingrese apellido: ");
-				System.out.print(" -");
-				apellidos = scn.next();
-
-				System.out.println("   Ingrese correo electronico: ");
-				System.out.print(" -");
-				correo = scn.next();
-
-				listaAlumno.add(alumno);
-				listaAlumno.add(alumno2);
-
-				listaAlumnos.setIdLista(1);
-				listaAlumnos.setListaAlumno(listaAlumno);
-
-				break;
-
-			case 2:
-
-				System.out.println("\t---Datos XML---");
-				//objectToXML(alumno);
-				listObjectTOXML(listaAlumnos);
-				break;
-
-			case 3:
-
-				System.out.println("\t---Datos JSON---");
-
-			}
-
-		} while (opcion != 4);
-
+		menuPrincipal();
 	}
 
 	public static void listObjectTOXML(ListaAlumnos listaAlumnos) {
@@ -120,6 +55,70 @@ public class AdapterApplication {
 			e.printStackTrace();
 
 		}
+	}
+
+	public static void agregarAlumno() {
+
+		System.out.println("Por favor ingrese los datos solicitados:");
+
+		System.out.println("   Ingrese carnet: ");
+		System.out.print(" -");
+		carnet = scn.nextInt();
+
+		System.out.println("   Ingrese nombre: ");
+		System.out.print(" -");
+		nombres = scn.next();
+
+		System.out.println("   Ingrese apellido: ");
+		System.out.print(" -");
+		apellidos = scn.next();
+
+		System.out.println("   Ingrese correo electronico: ");
+		System.out.print(" -");
+		correo = scn.next();
+
+		alumno = new Alumno(carnet, nombres, apellidos, correo);
+		listaAlumno.add(alumno);
+
+		menuPrincipal();
+	}
+
+	public static void menuPrincipal() {
+		do {
+
+			System.out.println(" ");
+			System.out.println("\t ---Menú para registrar datos---");
+			System.out.println("Escriba el número de la acción a realizar");
+			System.out.println("1. Ingresar datos Alumnos");
+			System.out.println("2. Mostrar datos en XML");
+			System.out.println("3. Mostrar datos en JSON");
+			System.out.println("4. Salir");
+			System.out.print("--Opción:");
+			opcion = scn.nextInt();
+
+			switch (opcion) {
+
+			case 1:
+
+				agregarAlumno();
+
+				break;
+
+			case 2:
+
+				System.out.println("\t---Datos XML---");
+				objectToXML(alumno);
+				//listObjectTOXML(listaAlumno);
+
+				break;
+
+			case 3:
+
+				System.out.println("\t---Datos JSON---");
+
+			}
+
+		} while (opcion != 4);
 	}
 
 	public static void objectToXML(Alumno alumno) {
